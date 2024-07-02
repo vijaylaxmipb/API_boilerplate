4249c99e9c814a56771722df599c850f479215a7
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-07-02 16:58:29 +0000

    Add display status to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ const resultsModal = new bootstrap.Modal(documnet.getElementById("resultsModal")
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
-async function ger Status(e) {
-    
+async function getStatus(e) {
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -13,7 +13,21 @@ async function ger Status(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    }else{
+        throw new Error(data.error);
     }
 
+}
+
+function displayStatus(data){
+    
+    let heading = API Key Status";
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
